bcc594ed84fa9428a2e8019399eb0e8f76c1dea8
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-14 10:46:30 +0530

    uploading list rendering task

---
+++ list_rendering/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ListRendering } from './Components/listRendering';
+
+function App() {
+  return (
+    <>
+    <ListRendering></ListRendering>
+    </>
+  );
+}
+
+export default App;
