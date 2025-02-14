97ec245b41e1879d31dae8f4303305668d781c67
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-14 10:44:26 +0530

    uploading College Alumnus task

---
+++ college_alumnus/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Figma } from './Components/Figma/Figma';
+
+function App() {
+  return (
+    <div className="App">
+      <Figma/>
+    </div>
+  );
+}
+
+export default App;
