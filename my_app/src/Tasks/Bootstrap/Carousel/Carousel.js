2bece24d464ec2eb9fb7fa50cf183d82fb065a33
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-21 17:08:28 +0530

    updating my-app

--- my_app/src/Tasks/Bootstrap/Carousel/Carousel.js
+++ my_app/src/Tasks/Bootstrap/Carousel/Carousel.js
@@ -9,10 +9,10 @@ export const Carousel1 = () => {
                 <img src={img} alt="" className='img'/>
             </Carousel.Item>
             <Carousel.Item>
-                <img src={img} alt="" className='img'/>
+                <img src="https://th.bing.com/th/id/R.fcfde7d19cdac423e74cee3b206fb238?rik=OEkFwOI1Zuu7Lg&riu=http%3a%2f%2f4.bp.blogspot.com%2f-kA-ZJ2602W8%2fUYfkL8PnytI%2fAAAAAAAACJ8%2fnQMW84AwJQ0%2fs1600%2fScreen%2bshot%2b2013-05-06%2bat%2b1.10.33%2bPM.png&ehk=mIT8yIKs%2bNHIqC7e05p0yRos7oQDw72FQEbatilckPU%3d&risl=&pid=ImgRaw&r=0" alt="" className='img'/>
             </Carousel.Item>
             <Carousel.Item>
-                <img src={img} alt="" className='img'/>
+                <img src="https://echt.com.au/cdn/shop/files/Mobile_-_w_image_2000x1904.jpg?v=1710288868" alt="" className='img'/>
             </Carousel.Item>
         </Carousel>
     </>
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Task1 } from './Tasks/task1/task1';
-// import { Navbar1 } from './Tasks/Bootstrap/Navbar/Navbar';
-// import { Carousel1 } from './Tasks/Bootstrap/Carousel/Carousel';
-// import { ListRendering } from './Tasks/Bootstrap/Listrendering/listRendering';
+// import { Task1 } from './Tasks/task1/task1';
+import { Navbar1 } from './Tasks/Bootstrap/Navbar/Navbar';
+import { Carousel1 } from './Tasks/Bootstrap/Carousel/Carousel';
+import { ListRendering } from './Tasks/Bootstrap/Listrendering/listRendering';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Navbar1/>
+    <Navbar1/>
     <Carousel1/>
-    <ListRendering/> */}
+    <ListRendering/>
     {/* <App /> */}
-    <Task1></Task1>
+    {/* <Task1></Task1> */}
   </React.StrictMode>
 );
 
